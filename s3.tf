8bc5dc9e4b9e246938374be0305b32de74f16bae
ParatalaKundana <133763044+ParatalaKundana@users.noreply.github.com>
2025-01-23 16:28:12 +0530

    Update s3.tf

--- s3.tf
+++ s3.tf
@@ -1,6 +1,6 @@
 # Create an S3 Bucket
 resource "aws_s3_bucket" "example_bucket" {
-  bucket = "terraform-0001-swiggy-902"
+  bucket = "terraform-0001-swiggy"
 
   # Enable versioning
   versioning {
